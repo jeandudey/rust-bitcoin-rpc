047d42a6fda7700ab654b3b03a7363d7b9d383f4
Dawid Ciężarkiewicz <dawid@bitgo.com>
2018-10-29 10:59:39 -0700

    Fix getblock

--- src/lib.rs
+++ src/lib.rs
@@ -257,7 +257,7 @@ impl BitcoinRpc {
         let v: String = rpc_request!(
             &self.client,
             "getblock".to_string(),
-            vec![block_hash.to_string().into(), "0".into()]
+            vec![block_hash.to_string().into(), 0.into()]
         );
         Ok(v)
     }
